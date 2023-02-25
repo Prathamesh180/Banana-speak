a06c59cc008dfd7a1e59184f0eec9f0c909786f2
Prathamesh180 <85394583+Prathamesh180@users.noreply.github.com>
2023-02-25 18:37:15 +0530

    structure_done

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var btntranslate = document.querySelector("#btn-translate");
-var textarea = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+var textOutput = document.querySelector("#output");
 
-function btnclicked() {
-    console.log("button clicked.");
-    console.log("input",textarea.value);
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(inputValue){
+    return serverURL + "?" + "text=" + inputValue
 }
 
-btntranslate.addEventListener("click",btnclicked);
-textarea.addEventListener()
+
+function errorHandler(error){
+    console.log("error occured" + error);
+    alert("something went wrong please try after sometime")
+}
+
+function btnclicked() {
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        textOutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",btnclicked);
